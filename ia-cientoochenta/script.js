1f2e6c08c9538bb9429500c22ec1c3219855a15b
Track1at01 <ricardoawnil@gmail.com>
2025-12-20 00:46:05 -0500

    ARREGLAO

--- ia-cientoochenta/script.js
+++ ia-cientoochenta/script.js
@@ -1,37 +1,30 @@
- const contenedor = document.querySelector(".contenedor");
-const btnsignin = document.getElementById ("btn-sign-in");
-const btnsignup = document.getElementById ("btn-sign-up");
+const contenedor = document.querySelector('.contenedor');
+const btnsignin = document.getElementById('btn-sign-in');
+const btnsignup = document.getElementById('btn-sign-up');
 
-btnsignin.addEventListener("click", ()=>{
-contenedor.classList.remove("toggle")
+btnsignin.addEventListener('click', () => {
+  contenedor.classList.remove('toggle');
+});
 
-
-})
-
-btnsignup.addEventListener("click", ()=>{
-contenedor.classList.add("toggle")
-
-    
-})
-
-
-
+btnsignup.addEventListener('click', () => {
+  contenedor.classList.add('toggle');
+});
 
 const apiUrl = 'http://localhost:3000/users';
 
-function registerUsers(){
-    const newUser ={
-        name: document.getElementById('reg-name').value,
-         email: document.getElementById('reg.email').value,
-password: document.getElementById('reg-pass').value
-        };
+function registerUsers() {
+  const newUser = {
+    name: document.getElementById('reg-name').value,
+    email: document.getElementById('reg-email').value,
+    password: document.getElementById('reg-pass').value
+  };
 
-        fetch(apiUrl, {
-            methodm : 'POST',
-            headers: {
-                'ContentType': 'application/json'
-            },
-            body: JSON.stringify(newUser)
-        });
-        alert('Usuario agregado')
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      ContentType: 'application/json'
+    },
+    body: JSON.stringify(newUser)
+  });
+  alert('Usuario agregado');
 }
